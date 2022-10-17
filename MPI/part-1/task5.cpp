c98e800042ffab5035e76fc590cf664ad1211bb8
renett_t <rtapkina@gmail.com>
2022-10-17 14:19:52 +0300

    Task 1.5 done YAY

--- MPI/part-1/task5.cpp
+++ MPI/part-1/task5.cpp
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <mpi.h>
-#include <map>
+#include <vector>
 #include <cmath>
+#include <unordered_map>
 
 const int RAND_MAX_VALUE = 10;
-const int ARRAY_SIZE = 9;
+const int ARRAY_SIZE = 10;
 const int A = 2;
 const int B = 3;
+typedef std::vector<int> Data;
+
+void print_map(const std::unordered_map<int, Data>& m)
+{
+    for (const auto& [key, value] : m)
+        std::cout << '[' << key << "] = {" << value[0] << ", " << value[1] << "}; ";
+    std::cout << '\n';
+}
 
 int main(int argc, char *argv[]) {
     int rank, size;
-    int tag = 1;
+    int tag_from_main = 40;
+    // int tag_from_main_y = 41;
+    int tag_answer = 42;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
@@ -26,6 +37,7 @@ int main(int argc, char *argv[]) {
             x[i] = rand() % RAND_MAX_VALUE;
             y[i] = rand() % RAND_MAX_VALUE;
         }
+
         printf("Initialized vectors x and y.\n X: ");
         for (int i = 0; i < ARRAY_SIZE; i++) {
             printf("%d ", x[i]);
@@ -36,49 +48,54 @@ int main(int argc, char *argv[]) {
         }
         printf("\n\n");
 
+        int BLOCK_SIZE = ARRAY_SIZE / (size - 1) + (ARRAY_SIZE % (size - 1) != 0);
+        std::unordered_map<int, Data> mp; // карта, которая хранит инф-цию о том, какие индексы обрабатывает i-тый процесс. 
+        // key - номер процесса, value  - двумерный массив, у коготого 
+        // arr[0] = начальный индекс, обрабатываемый данным процессом, arr[1] = кол-во обрабатываемых данным процессом элементов.
+       
         int receiver = 1;
-        int BLOCK_SIZE = std::ceil((rank - 1.0) / (0.0 + ARRAY_SIZE));
-        
-        std::map<int, int[2]> mp; 
-        // карта, которая хранит инф-цию о том, какие индексы обрабатывает i-тый процесс. key - номер процесса, value  - двумерный массив, у коготого arr[0] = начальный индекс, обрабатываемый данным процессом, arr[1] = кол-во обрабатываемых данным процессом элементов.
-
         for (int k = 0; k < ARRAY_SIZE; k += BLOCK_SIZE) {
             int block_size = std::min(BLOCK_SIZE, ARRAY_SIZE - k);
-            printf("    sending %d elements (from %d to %d) to process №%d\n", block_size, k, k + block_size - 1, receiver);
-            int data[2] = {k , block_size};
-            mp[receiver] = data;
-            MPI_Send(&x[k], block_size, MPI_INT, receiver, tag, MPI_COMM_WORLD);
-            MPI_Send(&x[k], block_size, MPI_INT, receiver, tag, MPI_COMM_WORLD);
+            printf("Sending %d elements (from %d to %d) to process №%d\n", block_size, k, k + block_size - 1, receiver);
+            mp[receiver] = {k , block_size};
+            MPI_Send(&x[k], block_size, MPI_INT, receiver, tag_from_main, MPI_COMM_WORLD);
+            MPI_Send(&y[k], block_size, MPI_INT, receiver, tag_from_main, MPI_COMM_WORLD);
             receiver++;
         }
         printf("Process %d sent all parts of vectors x and y. Waiting for results from another processes!.\n", sender);
+        // print_map(mp);
 
         int z[ARRAY_SIZE];
-        for (int k = 1; k < size; k++) {
+        int waitingCount = size - 1;
+        while(waitingCount > 0) {
             // получить сообщение от других процессов
             MPI_Status status;
-            MPI_Probe(receiver, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+            MPI_Probe(MPI_ANY_SOURCE, tag_answer, MPI_COMM_WORLD, &status);
             int count;
             MPI_Get_count(&status, MPI_INT, &count);
+            printf("Received answer from %d process in main process.\n", status.MPI_SOURCE);
             int received[count];
             MPI_Recv(&received, count, MPI_INT, status.MPI_SOURCE, status.MPI_TAG, MPI_COMM_WORLD, &status);
 
             // узнать кто прислал, что прислал и заполнить массив z
-            int data[2] = mp[status.MPI_SOURCE];
-            for (int j = data[0], i = 0; j < data[1]; j++, i++) {
+            Data data = mp.at(status.MPI_SOURCE);
+            int i = 0;
+            for (int j = data[0]; j < data[0] + data[1]; j++, i++) {
                 z[j] = received[i];
             }
+            waitingCount--;
         }
 
-        printf("\n Got result! Z: ");
+        printf("\nGot result! Z: ");
         for (int i = 0; i < ARRAY_SIZE; i++) {
             printf("%d ", z[i]);
         }
         printf("\n\n");
     }
     if (rank != sender) {
+
         MPI_Status status;
-        MPI_Probe(sender, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+        MPI_Probe(sender, tag_from_main, MPI_COMM_WORLD, &status);
         int count;
         MPI_Get_count(&status, MPI_INT, &count);
         int x[count];
@@ -86,17 +103,27 @@ int main(int argc, char *argv[]) {
         MPI_Recv(&x, count, MPI_INT, sender, status.MPI_TAG, MPI_COMM_WORLD, &status);
         MPI_Recv(&y, count, MPI_INT, sender, status.MPI_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
+        printf("\x1B[33m    Received elements in process №%d. Elements: X: ", rank);
+        for (int i = 0; i < count; i++) {
+            printf("%d ", x[i]);
+        }
+        printf("; Y: ");
+        for (int i = 0; i < count; i++) {
+            printf("%d ", y[i]);
+        }
+        printf("\033[0m\n");
+
         int z[count];
         for (int i = 0; i < count; i++) {
             z[i] = A * x[i] + B * y[i];
         }
-        printf("    Calculated part of z in process №%d. Elements: ", rank);
+        printf("\x1B[33m    ! Calculated part of z in process №%d. Elements: ", rank);
         for (int i = 0; i < count; i++) {
             printf("%d ", z[i]);
         }
-        printf("\n");
+        printf("\033[0m\n");
 
-        MPI_Send(&y[0], count, MPI_INT, sender, tag, MPI_COMM_WORLD);
+        MPI_Send(&z[0], count, MPI_INT, sender, tag_answer, MPI_COMM_WORLD);
     }
 
     MPI_Finalize();
